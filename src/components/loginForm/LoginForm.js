c134b717a5464ace0fa240f15e1665c82000cd79
ramanKylient <radharaman.ratre@kylient.com>
2024-03-14 08:01:48 +0530

    Add Login functionality

---
+++ src/components/loginForm/LoginForm.js
@@ -0,0 +1,89 @@
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import {
+  Avatar,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Container,
+  Box,
+  TextField,
+  Typography,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { signIn } from "../../utilities/service/auth";
+
+function LoginForm() {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const user = {
+      email: data.get("email"),
+      password: data.get("password"),
+    };
+
+    try {
+      const signInResponse = await signIn(user);
+      console.log(signInResponse); // Do something with the signIn response
+    } catch (error) {
+      console.error("Sign in failed:", error); // Handle sign in error
+    }
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
+
+export default LoginForm;
---
+++ src/utilities/service/auth.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+// Exporting signIn function
+export const signIn = async (user) => {
+  try {
+    // Make a POST request to the server using axios
+    const response = await axios.post(
+      `${process.env.REACT_APP_BASE_URL}/auth/sign-in`,
+      user
+    ); // Use process.env.REACT_APP_BASE_URL
+    // If the request is successful, return the response data
+    return response.data;
+  } catch (error) {
+    // If there's an error, throw it to be caught by the caller
+    throw error;
+  }
+};
