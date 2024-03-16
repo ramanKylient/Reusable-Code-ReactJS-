ef5c70b7f1ed503855341db6fc3fbb90a56c14a3
ramanKylient <radharaman.ratre@kylient.com>
2024-03-16 15:10:16 +0530

    Revert changes to authentication and routing.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import LoginForm from "./components/loginForm/LoginForm";
-import {
-  PublicRoutes,
-  PrivateRoutes,
-} from "./utilities/protectedRoutes/authRoutes";
-import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route element={<PrivateRoutes />}>
-          <Route path="/" exact element={<HomePage />} />
-        </Route>
-        <Route element={<PublicRoutes />}>
-          <Route path="/login" element={<LoginForm />} />
-        </Route>
+        <Route path="/login" element={<LoginForm />} />
       </Routes>
     </>
   );
