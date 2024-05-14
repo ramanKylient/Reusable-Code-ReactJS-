9abfd79a68123c4ee4b03ee8d18527770a26c8ca
ramanKylient <radharaman.ratre@kylient.com>
2024-05-14 13:35:53 +0530

    Change API base URL in .env copy.local and update auth.js to use /auth/admin/sign-in endpoint

--- src/utilities/service/auth.js
+++ src/utilities/service/auth.js
@@ -5,7 +5,7 @@ export const signIn = async (user) => {
   try {
     // Make a POST request to the server using axios
     const response = await axios.post(
-      `${process.env.REACT_APP_BASE_URL}/auth/sign-in`,
+      `${process.env.REACT_APP_BASE_URL}/auth/admin/sign-in`,
       user
     );
     // If the request is successful, return the response data
